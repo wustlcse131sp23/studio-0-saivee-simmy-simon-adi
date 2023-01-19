718407a9d59f21ad492dd5636313b699b7e7f3da
saiveeahuja <saiveeahuja@localhost>
2023-01-19 12:10:51 -0600

    hello

--- src/studio0/Message.java
+++ src/studio0/Message.java
@@ -3,6 +3,6 @@ package studio0;
 public class Message {
 
 	public static void main(String args[]) {
-		System.out.println("hey cunts");
+		System.out.println("hey pals come to the apple store w me at 4pm");
 	}
 }
